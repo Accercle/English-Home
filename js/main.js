199e64964921ba1c410761760b815279f7ca9fba
Maria Konova <Cinderrel@yandex.ru>
2024-12-19 18:19:11 +0300

    add main page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false, 
+  },
+
+});
